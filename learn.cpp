03336bc4e978f3e2a8799a59652b1a0490cca803
jhamiltonjunior <josehamiltonsantosjunior@gmail.com>
2023-06-05 01:53:17 -0300

    news learns

--- learn.cpp
+++ learn.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
 
 int main() {
-  printf("Hello World!\n");
+  cout << "Hello World!" << endl;
+
+  system("pause");
+
   return(0);
 }
